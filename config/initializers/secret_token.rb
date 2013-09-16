06678568057768b0d30044870671ab9c0ee73571
Capybara <git.capybara@gmail.com>
2013-09-16 18:46:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'f5d4c8361c3a8d5d1b49bb0fa14a53603b6ef6f18b03007d71b946b9758d5a3a167b2893144a63c5bf61009ca8b2080dbb3f95fd156f0722b4419a9d4e0da4e3'
